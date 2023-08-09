9f6f9ccf10c118e1f6c1351d685d7c97dbed29d5
Daniel Cunningham <138261809+danielxcastle@users.noreply.github.com>
2023-08-09 22:22:12 +0000

    finished bonus

--- src/app.js
+++ src/app.js
@@ -7,7 +7,15 @@
     for(let b = 0; b < middlewords.length; b++){
       for(let c = 0; c < thirdwords.length; c++){
         for(let d = 0; d < extension.length; d++){         
-          console.log(`${firstwords[a]}${middlewords[b]}${thirdwords[c]}.${extension[d]}`);           
+          // check if whole domain name ends with extension
+          let domainName = `${firstwords[a]}${middlewords[b]}${thirdwords[c]}`;
+          if(domainName.endsWith(extension[d])){
+              //if true we remove the last extension.length characters from domain name
+            domainName = domainName.slice(0, - extension[d].length);
+          }
+          //print domain name with the extension
+          
+          console.log(`${domainName}.${extension[d]}`);           
       }
     }
   }
