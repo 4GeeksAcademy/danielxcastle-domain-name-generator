dc780f72f86273d331de06d476413b0e87e84704
Daniel Cunningham <138261809+danielxcastle@users.noreply.github.com>
2023-08-09 15:26:25 +0000

    domain name gen

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+  let firstwords = ["Cool", "Sick", "The", "Original", "Amazing"];
+  let middlewords = ["student", "skater", "surfer", "dog", "cat"];
+  let thirdwords = ["profile", "portfolio", "page", "webpage", "us"];
+  let extension = ["com", "co.uk", "org", "edu", "gov", "us"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for(let a = 0; a < firstwords.length; a++){
+    for(let b = 0; b < middlewords.length; b++){
+      for(let c = 0; c < thirdwords.length; c++){
+        for(let d = 0; d < extension.length; d++){         
+          console.log(`${firstwords[a]}${middlewords[b]}${thirdwords[c]}.${extension[d]}`);           
+      }
+    }
+  }
+}
